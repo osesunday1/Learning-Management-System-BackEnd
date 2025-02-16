5b13a72bc1b9d93e9b3cf3fb0d75b043a77217ba
osesunday1 <ose.sunday@gmail.com>
2025-02-16 17:48:50 -0500

    webhooks

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -1,54 +1,49 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
-import HttpError from "../utils/httpError.js";
 
 
 // API Controller Function to Manage Clerk User with database
-
 export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    
-        await whook.verify(JSON.stringify(req.body), {
+
+        const payload = req.body; // Use raw body data
+        await whook.verify(payload, {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
         });
-    
-        const { data, type } = req.body;
+
+        const { data, type } = payload;
 
         switch (type) {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
-                    email: data.email_addresses[0].email_address,
-                    name: data.first_name + " " + data.last_name,
+                    email: data.email_addresses[0].email_address, // Fixed key name
+                    name: `${data.first_name} ${data.last_name}`,
                     imageUrl: data.image_url,
                 };
                 await User.create(userData);
-                res.json({})
-                break;
+                return res.json({});
             }
-           case 'user.updated': {
-            const userData = {
-                email: data.email_address[0].email_address,
-                name: data.first_name + " " + data.last_name,
-                imageUrl: data.image_url,
-            };
-            await User.findByIdAndUpdate(data.id, userData);
-            res.json({});
-            break;
-        }
-        case 'user.deleted': {
-            await User.findByIdAndDelete(data.id);
-            res.json({});
-            break;
-        }
-        
-        default:
-            break;
+            case 'user.updated': {
+                const userData = {
+                    email: data.email_addresses[0].email_address, // Fixed key name
+                    name: `${data.first_name} ${data.last_name}`,
+                    imageUrl: data.image_url,
+                };
+                await User.findByIdAndUpdate(data.id, userData);
+                return res.json({});
+            }
+            case 'user.deleted': {
+                await User.findByIdAndDelete(data.id);
+                return res.json({});
+            }
+            default:
+                return res.status(400).json({ error: "Unhandled event type" });
         }
     } catch (error) {
-        return next(new HttpError(`${error}`, 404));
+        return res.status(404).json({ error: error.message }); // Fixed error handling
     }
-}
+};
