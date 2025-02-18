b50d16490814acb1b79fa2815f96a00d0c273d4a
osesunday1 <ose.sunday@gmail.com>
2025-02-17 23:17:52 -0500

    ddsf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks'
+import { clerkWebhooks } from './controllers/webhooks.js'
 
 // Initialize Express
 const app = express()
